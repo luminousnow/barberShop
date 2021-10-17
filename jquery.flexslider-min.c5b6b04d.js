06e9b3840368a155b6e30367510aaa460024dab7
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2021-10-17 22:44:36 +0300

    Updates

--- jquery.flexslider-min.c5b6b04d.js
+++ jquery.flexslider-min.c5b6b04d.js
@@ -555,7 +555,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57014" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58117" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
